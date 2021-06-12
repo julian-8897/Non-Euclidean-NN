c3c728068d78cce88c0b174c771536a7faa744e9
julian8897-swansea <78489417+julian8897-swansea@users.noreply.github.com>
2021-06-13 00:19:37 +0100

    Mobius Operations update

--- mobius_ops.py
+++ mobius_ops.py
@@ -1,1 +1,26 @@
+"""
+Mobius operations for the Poincare model
+"""
+
 import numpy as np
+from numpy import linalg
+
+
+def m_add(x, y):
+    numer = (1.0 + 2.0 * np.dot(x, y) + linalg.norm(y)**2) * \
+        x + (1.0 - linalg.norm(x)**2) * y
+    denom = 1.0 + 2.0 * np.dot(x, y) + linalg.norm(x)**2 * linalg.norm(y)**2
+    return numer/denom
+
+
+def m_scalar_mul(r, x):
+    res = np.tanh(r * np.arctanh(linalg.norm(x))) * (x/linalg.norm(x))
+    return res
+
+
+def m_vector_mul(M, x):
+    Mx = M.dot(x)
+    norm_Mx = linalg.norm(Mx)
+    norm_x = linalg.norm(x)
+    res = np.tanh((norm_Mx/norm_x) * np.arctanh(norm_x)) * (Mx/norm_Mx)
+    return res
