e32a052c1caf666ebc5d7376b5705d8d30feafaf
julian8897-swansea <78489417+julian8897-swansea@users.noreply.github.com>
2021-06-13 00:43:22 +0100

    Update

--- mobius_test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import mobius_ops.py
-
-for i in range(0, 10000):
-    a = np.random.uniform(low=-0.01, high=0.01, size=1)
-    b = np.random.uniform(low=-0.01, high=0.01, size=1)
-
-    res = m_add(-a, b)
-    print(res)
