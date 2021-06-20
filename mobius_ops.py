8337b0a531209ce47ad0a5723f843081ff120009
julian8897-swansea <78489417+julian8897-swansea@users.noreply.github.com>
2021-06-21 00:40:53 +0100

    Mobius operations: added distance function

--- mobius_ops.py
+++ mobius_ops.py
@@ -1,5 +1,5 @@
 """
-Mobius operations for the Poincare model
+Useful operations for the Poincare model
 """
 
 import numpy as np
@@ -7,6 +7,7 @@ from numpy import linalg
 
 
 def m_add(x, y):
+    # Mobius addition operation
     numer = (1.0 + 2.0 * np.dot(x, y) + linalg.norm(y)**2) * \
         x + (1.0 - linalg.norm(x)**2) * y
     denom = 1.0 + 2.0 * np.dot(x, y) + linalg.norm(x)**2 * linalg.norm(y)**2
@@ -14,13 +15,22 @@ def m_add(x, y):
 
 
 def m_scalar_mul(r, x):
+    # Mobius scalar multiplication operation
     res = np.tanh(r * np.arctanh(linalg.norm(x))) * (x/linalg.norm(x))
     return res
 
 
 def m_vector_mul(M, x):
-    Mx = M.dot(x)
+    # Mobius vector multiplication operation
     norm_Mx = linalg.norm(Mx)
     norm_x = linalg.norm(x)
     res = np.tanh((norm_Mx/norm_x) * np.arctanh(norm_x)) * (Mx/norm_Mx)
     return res
+
+
+def dist_fn(x, y):
+    # closed-form expression of the distance function of the poincare model
+    numer = linalg.norm(x - y)**2
+    denom = (1.0 - linalg.norm(x)**2)*(1.0 - linalg.norm(y)**2)
+    distance = np.arccosh(1.0 + 2.0*numer/denom)
+    return distance
