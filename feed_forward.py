f9a358114a5a3266d1087aa1514b51269299cc49
julian8897-swansea <78489417+julian8897-swansea@users.noreply.github.com>
2021-07-02 00:27:34 +0100

    Feed forward update

--- feed_forward.py
+++ feed_forward.py
@@ -1,25 +1,27 @@
+import math
 import numpy as np
 import mobius
 import torch.nn as nn
+import torch
 
 
 class HypFF(nn.Module):
 
-    def __init__(self, weights, bias):
-        super(HypFF, self).__init__()
-        self.weights = weights
-        self.bias = bias
-        self.relu = nn.ReLU()
+    def __init__(self):
+        super().__init__()
+        torch.manual_seed(0)
+        self.weights1 = nn.Parameter(torch.randn(2, 2) / math.sqrt(2))
+        self.bias1 = nn.Parameter(torch.zeros(2))
+        self.weights2 = nn.Parameter(torch.randn(2, 4) / math.sqrt(2))
+        self.bias2 = nn.Parameter(torch.zeros(4))
 
-    def linear_transform(self, x):
-        #x: inputs
-        # Mobius version of the linear transformation
-        w_x = mobius.m_vector_mul(self.weights, x)
-        res = mobius.m_add(w_x, self.bias)
-        return res
+    def forward(self, X):
+        a1 = mobius.m_vector_mul(self.weights1, X) + self.bias1
+        h1 = a1.sigmoid()
+        a2 = mobius.m_vector_mul(self.weights2, X) + self.bias2
+        h2 = a2.exp()/a2.exp().sum(-1).unsqueeze(-1)
+        return h2
 
-    def forward(self, x):
-        #x: inputs
-        output = self.linear_transform(x)
-        output = self.relu(x)
-        return output
+    # Things to do:
+    # Project values into "hyperbolic values"
+    # find a way to test model
