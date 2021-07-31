8885824853b420f3fe93dc8e70247db2213e752f
Julian Chan <78489417+julian8897-swansea@users.noreply.github.com>
2021-07-31 23:12:08 +0100

    CIFAR10 Test

--- feed_forward.py
+++ feed_forward.py
@@ -8,25 +8,28 @@ import geoopt
 
 class HypFF(nn.Module):
 
-    def __init__(self):
+    def __init__(self, input_size, hidden_size1, hidden_size2, output_size):
         super(HypFF, self).__init__()
         # self.flatten = nn.Flatten()
-        self.fc1 = mobius.MobLinear(784, 512)
-        self.fc2 = mobius.MobLinear(512, 256)
-        self.fc3 = mobius.MobLinear(256, 10)
+        self.input_size = input_size
+        self.hidden_size1 = hidden_size1
+        self.hidden_size2 = hidden_size2
+        self.output_size = output_size
+        self.fc1 = mobius.MobLinear(self.input_size, self.hidden_size1)
+        self.fc2 = mobius.MobLinear(self.hidden_size1, self.hidden_size2)
+        self.fc3 = mobius.MobLinear(self.hidden_size2, self.output_size)
 
     def forward(self, x):
 
         ball = geoopt.PoincareBall()
         # x = self.flatten(x)
         # x = ball.projx(x)
-        x = ball.mobius_fn_apply(nn.LeakyReLU(), self.fc1(x))
-        x = ball.mobius_fn_apply(nn.LeakyReLU(), self.fc2(x))
+        x = ball.mobius_fn_apply(nn.Tanh(), self.fc1(x))
+        x = ball.mobius_fn_apply(nn.Tanh(), self.fc2(x))
         # x = self.fc1(x)
         # x = self.fc2(x)
-        x = self.fc3(x)
-        #x = ball.mobius_fn_apply(nn.LogSoftmax(dim=1), self.fc3(x))
-        # x = ball.logmap0(x)
+        x = ball.mobius_fn_apply(nn.LogSoftmax(dim=1), self.fc3(x))
+        #x = self.fc3(x)
         return x
 
     # Things to do:
@@ -36,20 +39,4 @@ class HypFF(nn.Module):
 
 
 ######################################################
-# ball = geoopt.PoincareBall()
-# model = HypFF()
-# params = list(model.parameters())
-# point = torch.Tensor([8.8])
-# input = ball.projx(point)
-# output = model(input)
 
-# print(model)
-# print("Output is :", output)
-# model.zero_grad()
-# output.backward(torch.randn(1))
-
-# target = torch.randn(1)
-# criteria = nn.MSELoss()
-
-# loss = criteria(output, target)
-# print(loss.grad_fn)
