de4cb0798d394c8cfea5d9d2e9394b9582a8c4df
Julian Chan <78489417+julian8897-swansea@users.noreply.github.com>
2021-08-05 17:23:20 +0100

    update: safe projection on poincare ball

--- mobius.py
+++ mobius.py
@@ -61,33 +61,3 @@ class MobLinear(torch.nn.Linear):
     #     if self.bias is not None:
     #         self.bias.zero_()
 
-
-# def m_add(x, y):
-#     # Mobius addition operation
-#     numer = (1.0 + 2.0 * np.dot(x, y) + linalg.norm(y)**2) * \
-#         x + (1.0 - linalg.norm(x)**2) * y
-#     denom = 1.0 + 2.0 * np.dot(x, y) + linalg.norm(x)**2 * linalg.norm(y)**2
-#     return numer/denom
-
-
-# def m_scalar_mul(r, x):
-#     # Mobius scalar multiplication operation
-#     res = np.tanh(r * np.arctanh(linalg.norm(x))) * (x/linalg.norm(x))
-#     return res
-
-
-# def m_vector_mul(M, x):
-#     # Mobius vector multiplication operation
-#     Mx = np.dot(M, x)
-#     norm_Mx = linalg.norm(Mx)
-#     norm_x = linalg.norm(x)
-#     res = np.tanh((norm_Mx/norm_x) * np.arctanh(norm_x)) * (Mx/norm_Mx)
-#     return res
-
-
-# def dist_fn(x, y):
-#     # closed-form expression of the distance function of the poincare model
-#     numer = linalg.norm(x - y)**2
-#     denom = (1.0 - linalg.norm(x)**2)*(1.0 - linalg.norm(y)**2)
-#     distance = np.arccosh(1.0 + 2.0*numer/denom)
-#     return distance
