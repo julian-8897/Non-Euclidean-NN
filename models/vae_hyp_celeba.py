cbdd62a3f8d7c4ee6e9004ba1fdc6376901ae1d7
Julian Chan <78489417+julian8897-swansea@users.noreply.github.com>
2021-11-21 22:12:59 +0000

    update

--- models/vae_hyp_celeba.py
+++ models/vae_hyp_celeba.py
@@ -61,8 +61,11 @@ class VariationalEncoder(nn.Module):
 
         std = logvar.mul(0.5).exp_()
         N = wrapped_normal.WrappedNormal(
-            torch.FloatTensor(mu).to(device), torch.FloatTensor(std).to(device), poincareball.PoincareBall(self.latent_dims))
-        #N.device = device
+            torch.as_tensor(mu, device=self.device), torch.as_tensor(std, device=self.device), poincareball.PoincareBall(self.latent_dims))
+        N.loc = N.loc.to(self.device)
+        N.loc.device = self.device
+        N._scale = N.scale.to(self.device)
+        
         #N.scale = torch.FloatTensor(N.scale).to(device)
         #z = torch.FloatTensor(z).to(self.device)
         z = N.rsample()
