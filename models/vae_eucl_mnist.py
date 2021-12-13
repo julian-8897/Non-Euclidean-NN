93c57fa58b313b80d5bc34b73b4c40d7fad06596
julian8897-swansea <78489417+julian8897-swansea@users.noreply.github.com>
2021-12-13 17:28:48 +0000

    update

--- models/vae_eucl_mnist.py
+++ models/vae_eucl_mnist.py
@@ -39,7 +39,7 @@ class VariationalEncoder(nn.Module):
         x = F.relu(self.linear1(x))
         mu = self.linear2(x)
         sigma = torch.exp(self.linear3(x))
-        # reparametrisation trick
+        # reparameterization trick
         z = mu + sigma*self.N.sample(mu.shape)
         # K-L divergence
         self.kl = (sigma**2 + mu**2 - torch.log(sigma) - 1/2).sum()
